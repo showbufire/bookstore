a65e53c78d0410cc2e882c747dd2b484e47709d8
Xiao Jiang <showbufire@gmail.com>
2014-09-06 11:00:42 -0700

    some bootstrap on show page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -71,6 +71,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:name, :cover, :creator_id)
+      params.require(:book).permit(:name, :cover, :desc, :creator_id)
     end
 end
